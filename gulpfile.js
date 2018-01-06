c4195a1d0e3bb6900d2af898d9d443ed155b4474
Bauyrzhan <baur3dn@gmail.com>
2018-01-06 14:06:53 +0600

    init

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var mjml = require('gulp-mjml');
+var browserSync = require('browser-sync').create();
+
+gulp.task('mjml', function() {
+    return gulp
+        .src('./index.mjml')
+        .pipe(mjml())
+        .pipe(gulp.dest('./'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('serve', ['mjml'], function() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+
+    gulp.watch('./index.mjml', ['mjml']);
+});
